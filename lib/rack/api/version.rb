0991589e5dfa15ba81302e632d5236080a89cf65
Nando Vieira <fnando.vieira@gmail.com>
2011-06-08 21:09:37 -0300

    Bump up version

--- lib/rack/api/version.rb
+++ lib/rack/api/version.rb
@@ -2,8 +2,8 @@ module Rack
   class API
     module Version
       MAJOR = 0
-      MINOR = 2
-      PATCH = 2
+      MINOR = 3
+      PATCH = 0
       STRING = "#{MAJOR}.#{MINOR}.#{PATCH}"
     end
   end
